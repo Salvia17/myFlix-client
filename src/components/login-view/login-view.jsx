740f43a7f3fe9ed55482f61239e5d5c1e8cf4dab
salvija <salvija13@gmail.com>
2021-03-13 08:17:37 +0000

    Creating login view component

---
+++ src/components/login-view/login-view.jsx
@@ -0,0 +1,50 @@
+import React, { useState } from 'react';
+import PropTypes from 'prop-types';
+import Form from 'react-bootstrap/Form';
+import Button from 'react-bootstrap/Button';
+
+import './login-view.scss';
+
+export function LoginView(props) {
+  const [username, setUsername] = useState('');
+  const [password, setPassword] = useState('');
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    console.log(username, password);
+    // Send a request to the server for authentication, then call props.onLoggedIn(username)
+    props.onLoggedIn(username);
+  };
+
+
+  return (
+    <Form className='login-form'>
+      <div className="login-header">
+        <h1 className='text-dark'>Log in</h1>
+      </div>
+      <Form.Group controlId="formUsername">
+        <Form.Label>Username:</Form.Label>
+        <Form.Control type="text" onChange={e => setUsername(e.target.value)} />
+      </Form.Group>
+
+      <Form.Group controlId="formPassword">
+        <Form.Label>Password:</Form.Label>
+        <Form.Control type="password" onChange={e => setPassword(e.target.value)} />
+      </Form.Group>
+      <Button variant="dark" type="submit" onClick={handleSubmit}>
+        Submit
+      </Button>
+    </Form>
+  );
+}
+
+LoginView.propTypes = {
+  user: PropTypes.shape({
+    username: PropTypes.string.isRequired,
+    pasword: PropTypes.string.isRequired
+  }),
+  onLoggedIn: PropTypes.func.isRequired,
+  onRegister: PropTypes.func
+};
+
+export default LoginView;
